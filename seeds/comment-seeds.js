4cc64a10a8670946e08bc0fc8aaf482af01d5698
bill6653 <billcrs512@gmail.com>
2023-03-16 00:15:59 -0500

    added my seeds

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,22 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+        user_id: 2,
+        post_id: 1,
+        comment_text: "great job of explaining."
+    },
+    {
+        user_id: 2,
+        post_id: 2,
+        comment_text: "Great! Awesome comparison."
+    },
+    {
+        user_id: 1,
+        post_id: 3,
+        comment_text: "I agree. It's a great way to simplify SQL queries."
+    }
+]
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
