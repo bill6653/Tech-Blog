dfd871fc5c4fb7b38f8de83fe199931f38886719
bill6653 <billcrs512@gmail.com>
2023-03-31 00:00:26 -0500

    changes for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,34 @@
 // Import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Create connection to database, pass in MySQL information for username and password
 let sequelize;
 
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+// }
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+  sequelize = new Sequelize(
+    "heroku_614641ae3b9689b",
+    "be123e735e8c7c",
+    "87c1550c",
+    {
+      host: "us-cdbr-east-06.cleardb.net",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
 }
 
 module.exports = sequelize;
